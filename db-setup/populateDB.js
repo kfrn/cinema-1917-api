806e16d803f66b150f130c959909afdd936d8aad
kfrn <knfrances@gmail.com>
2017-01-15 21:30:55 +1300

    Update populateDB FN; complete DB contents

--- db-setup/populateDB.js
+++ db-setup/populateDB.js
@@ -4,7 +4,7 @@ const extractIDs = require('./extractFromHTML').extractIDs
 const getFilmData = require('./getFilmData')
 
 function populateDB() {
-  getHTML("http://www.imdb.com/search/title?count=500&release_date=1917-01-01,1917-12-31&sort=year,asc")
+  getHTML("http://www.imdb.com/search/title?count=200&release_date=1917-01-01,1917-12-31&sort=year,asc&sort=year,asc&page=" + 28 + "&ref_=adv_nxt")
     .then (function(result) {
       var IMDbURLs = extractURLs(result)
       var IMDbIDs = extractIDs(IMDbURLs)
