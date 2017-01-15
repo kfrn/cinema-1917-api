bc245f2b86181ab26cc0303e475119ad4eff32a6
kfrn <knfrances@gmail.com>
2017-01-16 10:07:37 +1300

    Update config; delete unnecessary folder

--- functions/addFilmtoDB.js
+++
@@ -1,9 +0,0 @@
-var Knex = require('knex')
-var config = require('../knexfile')[ process.env.NODE_ENV || 'development' ]
-var knex = Knex(config)
-
-module.exports = addFilmtoDB
-
-function addFilmtoDB(film) {
-  return knex('films').insert(film)
-}
--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,13 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
       filename: './filmdata.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'grvexkdqsnsxfp',
-      password: 'bx7_kLgCpte-i28gKNNALKhm7M'
     },
-    pool: {
-      min: 2,
-      max: 100
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true
   },
 
   production: process.env.DATABASE_URL
 
-};
+}
