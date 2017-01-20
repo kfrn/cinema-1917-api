5cf1aa941f9a8422efafcb58a16e16c25f635860
Katherine Nagels <knfrances@gmail.com>
2017-01-21 11:13:31 +1300

    Fix route FNs for genres & countries

--- routes/helpers.js
+++ routes/helpers.js
@@ -3,12 +3,13 @@ const router = express.Router()
 
 const categoryData = require('../db-functions/helperFNs')
 
+// http://localhost:3000/api/v1/countries/
 router.get('/countries/', function(req, res, next) {
   categoryData.getCountries()
     .then(function(req) {
-      var countryList = req.map((elem) => elem.countries)
-      // TODO. Doesn't account for two word countries e.g. South Africa. 
-      countryList = countryList.join(" ").replace(/\,/g, '').split(" ").sort().filter((item, idx, self) => idx == self.indexOf(item))
+      var countryList = req.map((elem) => elem.countries).join(', ').split(", ").filter((item, idx, self) => idx == self.indexOf(item)).sort()
+      console.log({countryList})
+      res.status(200)
       res.json({countryList})
     })
     .catch(function(error) {
@@ -16,10 +17,12 @@ router.get('/countries/', function(req, res, next) {
     })
 })
 
+// http://localhost:3000/api/v1/genres/
 router.get('/genres/', function(req, res, next) {
   categoryData.getGenres()
     .then(function(req) {
       var genres = req.map((elem) => elem.genres).join(', ').split(", ").filter((item, idx, self) => idx == self.indexOf(item)).sort()
+      res.status(200)
       res.json({genres})
     })
     .catch(function(error) {
